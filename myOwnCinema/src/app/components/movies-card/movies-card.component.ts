9bba02896e89488e9a9d79f17848d9eeb7f7b577
dennerborba <dennerdeborba@gmail.com>
2025-06-30 15:07:39 -0300

    add projectCinema frontend

---
+++ myOwnCinema/src/app/components/movies-card/movies-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movies-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './movies-card.component.html',
+  styleUrl: './movies-card.component.css'
+})
+export class MoviesCardComponent {
+
+}
